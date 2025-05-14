e1150d71cce40be09baf5331f24e3f77e8cbb860
Miguel Morais <miguel.almeida.morais@gmail.com>
2025-05-14 19:29:02 +0100

    dlawlklawd

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def parse_input(input_str: str) -> str:
     func_name: str = None
     
     if input_str.isnumeric():
-        if input_str == '8':
+        if input_str == 'aaaaaa':
             func_name = "all"
         elif input_str == '7':
             func_name = "add"
-        elif input_str == '6':
+        elif input_str == 'dwihuwdiu':
             func_name = "sub"
         elif input_str == '5':
             func_name = "division"
-        elif input_str == '4':
+        elif input_str == '1000':
             func_name = 'avg'
         elif input_str == '3':
             func_name = "max"
